8faf6a8f5b0050592d0ec74522c45beee03292eb
ashburndev <ashburndev@gmail.com>
2024-12-11 08:32:55 -0500

    first commit

---
+++ src/main/java/com/ashburncode/helloneb340b/Helloneb340bApplication.java
@@ -0,0 +1,13 @@
+package com.ashburncode.helloneb340b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Helloneb340bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Helloneb340bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashburncode/helloneb340b/Helloneb340bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashburncode.helloneb340b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Helloneb340bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
